0c2dc0b36210a2c6b04eafc2fe2efa753271bb8b
AaryanSwami <swamiaaryan17@gmail.com>
2025-03-04 14:30:45 +0530

    Fixed footer links for HashRouter

--- aarav/src/components/Footer.jsx
+++ aarav/src/components/Footer.jsx
@@ -16,14 +16,12 @@ const Footer = () => {
           {/* Navigation Links */}
           <nav className="my-4 md:my-0">
   <ul className="flex space-x-6">
-    <li><Link to="/#/" className="hover:text-blue-500">Home</Link></li>
-    <li><Link to="/#/all-shorts" className="hover:text-blue-500">Shorts</Link></li>
-    <li><Link to="/#/about-page" className="hover:text-blue-500">About</Link></li>
-    <li><Link to="/#/contact-page" className="hover:text-blue-500">Contact</Link></li>
+    <li><Link to="/" className="hover:text-blue-500">Home</Link></li>
+    <li><Link to="/all-shorts" className="hover:text-blue-500">Shorts</Link></li>
+    <li><Link to="/about-page" className="hover:text-blue-500">About</Link></li>
+    <li><Link to="/contact-page" className="hover:text-blue-500">Contact</Link></li>
   </ul>
 </nav>
-
-
 
 
           {/* Social Media Links */}
--- aarav/src/main.jsx
+++ aarav/src/main.jsx
@@ -6,8 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter> {/* ✅ Wrap App with HashRouter */}
+   
       <App />
-    </HashRouter>
+
   </StrictMode>
 );
