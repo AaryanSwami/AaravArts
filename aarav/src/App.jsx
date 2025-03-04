792dffc3be6d06ef78c3386603ac232fcd8133f2
AaryanSwami <swamiaaryan17@gmail.com>
2025-03-04 14:47:25 +0530

    Fixed

--- aarav/src/App.jsx
+++ aarav/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AboutPage from "./components/AboutPage";
 import AllShorts from "./components/AllShorts";
@@ -8,10 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="AaravArts/#/" element={<Home />} />
-        <Route path="AaravArts/#/all-shorts" element={<AllShorts />} />
-        <Route path="AaravArts/#/about-page" element={<AboutPage />} />
-        <Route path="AaravArts/#/contact-page" element={<ContactPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/all-shorts" element={<AllShorts />} />
+        <Route path="/about-page" element={<AboutPage />} />
+        <Route path="/contact-page" element={<ContactPage />} />
       </Routes>
     </Router>
   );
